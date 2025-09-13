394a0e2240754d921196caa79d6a7ee0c2e0f788
Franco Coria <110592868+FrancoJCoria@users.noreply.github.com>
2025-09-13 15:59:25 -0300

    Creacion del Proyecto

---
+++ src/main/java/com/mycompany/practico/gui6/PracticoGUI6.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practico.gui6;
+
+import com.mycompany.practico.gui6.gui.VistaProductos;
+
+/**
+ *
+ * @author FRANCO
+ */
+public class PracticoGUI6 {
+
+    public static void main(String[] args) {
+         java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new VistaProductos().setVisible(true);
+            }
+        });
+    }
+}
