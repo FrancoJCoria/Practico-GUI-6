5756ee36beae8f55adda394017c0b97336a469f6
Fernando Lorenzo <79378662+FerLorenzoR@users.noreply.github.com>
2025-09-13 18:18:51 -0300

    Update PracticoGUI6.java

--- src/main/java/com/mycompany/practico/gui6/PracticoGUI6.java
+++ src/main/java/com/mycompany/practico/gui6/PracticoGUI6.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.practico.gui6;
 
 
@@ -10,10 +6,7 @@ import com.mycompany.practico.gui6.gui.VistaProductos;
 import java.util.Set;
 import java.util.TreeSet;
 
-/**
- *
- * @author FRANCO
- */
+
 public class PracticoGUI6 {
 
     public static void main(String[] args) {
